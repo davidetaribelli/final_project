3f6a6f3653516a7f2f837aa47e6664c2af74a0c7
MathiasChirivi <chirivimathias3@gmail.com>
2023-08-23 22:03:38 +0200

    Add store and bcrypt($user['password'])

--- config/store.php
+++ config/store.php
@@ -12,8 +12,106 @@
             'phone' => '3384567789',
             'cachet' => '200',
             'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
-
-
-        ]
-    ]
+        ],
+        [
+            'name' => 'John',
+            'surname'=> 'Doe',
+            'email' => 'johndoe@example.com',
+            'password' =>'test123',
+            'img' => 'https://example.com/images/john.jpg',
+            'region' => 'Puglia',
+            'phone' => '1234567890',
+            'cachet' => '1200',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius veniam inventore numquam quia!'
+        ],
+        [
+            'name' => 'Laura',
+            'surname'=> 'Smith',
+            'email' => 'lsmith@example.com',
+            'password' =>'securepass',
+            'img' => 'https://example.com/images/laura.jpg',
+            'region' => 'Sicilia',
+            'phone' => '9876543210',
+            'cachet' => '800',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil cum officiis omnis tempore eveniet!'
+        ],
+        [
+            'name' => 'Michele',
+            'surname'=> 'Ricci',
+            'email' => 'mricci@example.com',
+            'password' =>'michele123',
+            'img' => 'https://example.com/images/michele.jpg',
+            'region' => 'Lombardia',
+            'phone' => '5678901234',
+            'cachet' => '400',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut labore excepturi maiores enim molestias?'
+        ],
+        [
+            'name' => 'Emma',
+            'surname'=> 'Johnson',
+            'email' => 'emma@example.com',
+            'password' =>'emma567',
+            'img' => 'https://example.com/images/emma.jpg',
+            'region' => 'Basilicata',
+            'phone' => '3456789123',
+            'cachet' => '980',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum alias magnam saepe ipsa rem deleniti.'
+        ],
+        [
+            'name' => 'Marco',
+            'surname'=> 'Bianchi',
+            'email' => 'mbianchi@example.com',
+            'password' =>'marco987',
+            'img' => 'https://example.com/images/marco.jpg',
+            'region' => 'Veneto',
+            'phone' => '6789012345',
+            'cachet' => '350',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad modi numquam rem dolores, repellendus dignissimos!'
+        ],
+        [
+            'name' => 'Sophia',
+            'surname'=> 'Miller',
+            'email' => 'sophia@example.com',
+            'password' =>'sophia567',
+            'img' => 'https://example.com/images/sophia.jpg',
+            'region' => 'Umbria',
+            'phone' => '4567890123',
+            'cachet' => '150',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, distinctio architecto rem eum adipisci aut.'
+        ],
+        [
+            'name' => 'Antonio',
+            'surname'=> 'Ferrari',
+            'email' => 'aferrari@example.com',
+            'password' =>'antonio123',
+            'img' => 'https://example.com/images/antonio.jpg',
+            'region' => 'Emilia-Romagna',
+            'phone' => '8901234567',
+            'cachet' => '740',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit labore voluptatibus aperiam voluptate rerum enim!'
+        ],
+        [
+            'name' => 'Isabella',
+            'surname'=> 'Lee',
+            'email' => 'isabella@example.com',
+            'password' =>'isabella567',
+            'img' => 'https://example.com/images/isabella.jpg',
+            'region' => 'Abruzzo',
+            'phone' => '6789123456',
+            'cachet' => '680',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, neque tempore ipsa repellat veritatis unde!'
+        ],
+        [
+            'name' => 'Alessio',
+            'surname'=> 'Martinez',
+            'email' => 'amartinez@example.com',
+            'password' =>'alessio123',
+            'img' => 'https://example.com/images/alessio.jpg',
+            'region' => 'Marche',
+            'phone' => '7890123456',
+            'cachet' => '310',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam aliquid incidunt voluptatem magnam cupiditate.'
+        ],
+    ];
+    
 ?>
