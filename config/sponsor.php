97ae780ced2a6ef7b01a1931d5ae34d94062fc14
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-08-24 12:21:05 +0200

    create ms sponsor

---
+++ config/sponsor.php
@@ -0,0 +1,20 @@
+<?php
+   return[
+        [
+            'price' => '2,99',
+            'duration' => '24',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero fugit, ea nisi aperiam nemo nesciunt, animi consequuntur quos quo sapiente at sequi. Voluptates ut provident maiores, eligendi natus consequatur.'
+        ],
+        [
+            'price' => '5,99',
+            'duration' => '72',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero fugit, ea nisi aperiam nemo nesciunt, animi consequuntur quos quo sapiente at sequi. Voluptates ut provident maiores, eligendi natus consequatur.'
+        ],
+        [
+            'price' => '9,99',
+            'duration' => '144',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero fugit, ea nisi aperiam nemo nesciunt, animi consequuntur quos quo sapiente at sequi. Voluptates ut provident maiores, eligendi natus consequatur.'
+        ],
+    ]
+    
+?>
