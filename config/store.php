9c97224c10708126386b70b3d7b190a14dde77a7
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-08-24 10:08:11 +0200

    store

--- config/store.php
+++ config/store.php
@@ -112,6 +112,126 @@
             'cachet' => '310',
             'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam aliquid incidunt voluptatem magnam cupiditate.'
         ],
+        [
+            'name' => 'Amar',
+            'surname'=> 'Corda',
+            'email' => 'amarcorda838@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_mariella-papanaga-7587_114a9_FQLY9li.jpeg',
+            'region' => 'Lombardia',
+            'phone' => '338456725',
+            'cachet' => '150',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Mariella',
+            'surname'=> 'Papanaga',
+            'email' => 'gpaolini98@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_cantante-lirica-soprano-professionista-12863_FokLYge.jpeg',
+            'region' => 'Campania',
+            'phone' => '3884567789',
+            'cachet' => '250',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Giacomo',
+            'surname'=> 'Paolini',
+            'email' => 'gpaolini98@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.thomann.de/blog/wp-content/uploads/2017/01/10-conseils-pour-devenir-musicien-pro.jpg',
+            'region' => 'Lazio',
+            'phone' => '3384567789',
+            'cachet' => '200',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Gabry',
+            'surname'=> 'Sax',
+            'email' => 'saxgabri@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/project_thumb_928a9a69bf.jpeg',
+            'region' => 'Calabria',
+            'phone' => '3217567789',
+            'cachet' => '350',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Swingin',
+            'surname'=> 'Cats',
+            'email' => 'cats@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_Raggi-Brillante-4et-22_1590e_cW5WKEz.jpeg',
+            'region' => 'Emilia Romagna',
+            'phone' => '3456412196',
+            'cachet' => '500',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Music',
+            'surname'=> 'Live ',
+            'email' => 'musiclive@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_Estate-Live-Music-7478_b9c7f_lVyfsEI.jpeg',
+            'region' => 'Veneto',
+            'phone' => '3387631258',
+            'cachet' => '425',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Men on The',
+            'surname'=> 'Moon',
+            'email' => 'MenonThe@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/project_thumb_9697520c26.jpeg',
+            'region' => 'Veneto',
+            'phone' => '344196789',
+            'cachet' => '390',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'ALFA',
+            'surname'=> 'DJ',
+            'email' => 'ALFA@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/project_thumb_427016c7cc.jpeg',
+            'region' => 'Abruzzo',
+            'phone' => '3384567789',
+            'cachet' => '800',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Swing Jazz',
+            'surname'=> 'Trio',
+            'email' => 'triojazz@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_Swing-Jazz-Trio-5718_5e30d.jpeg',
+            'region' => 'Emilia Romagna',
+            'phone' => '33145697825',
+            'cachet' => '200',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
+        [
+            'name' => 'Marco',
+            'surname'=> 'Zorzi',
+            'email' => 'marcozorzi8@gmail.com',
+            'password' => 'root',
+            'img' => 'https://www.musiqua.it/djmedia/cropped_main_band_pictures/cropped_resized_Marco-Pianist-6792_1d31c_CdEEgtj.jpeg',
+            'region' => 'Veneto',
+            'phone' => '34478912645',
+            'cachet' => '850',
+            'experience' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus laboriosam, quae distinctio voluptas facere eaque totam rem libero quisquam architecto et non voluptatem cupiditate voluptates in id repudiandae vitae pariatur.',
+            
+        ],
     ];
     
 ?>
