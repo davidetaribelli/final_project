d2a39402f4cffe9b14ddcf6516d778e85ba0fbc4
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-08-25 18:58:32 +0200

    style page index.blade

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,20 +2,52 @@
 
 @section('content')
 
-<div class="container-fluid mt-4">
-    <div class="row justify-content-between">
-        <h2>Il tuo Profilo</h2>
-        <h1>{{  $user->name }}</h1>
-        <p>{{  $user->surname }}</p>
-        <h2>{{  $user->email }}</h2>
-        Numero Di telefono:<h2>{{  $user->phone }}</h2> 
-        Prezzo:<h2>{{  $user->cachet }}</h2> 
-        <img src={{$user->img}} alt={{$user->name}}>
+<div class="container-fluid mt-2">
+    <div class="row">
+        <h1 class="p-0">Il tuo Super Profilo</h1>
+        {{-- img. --}}
+        <div class="col-6 p-0">
+            <img class="rounded-start w-100 shadow bg-body" src={{$user->img}} alt={{$user->name}}>
+        </div>
+
+        {{-- dati personali. --}}
+        <div class="col-6">
+            <h4 class="p-2">Dati Personali</h4>
+
+            <div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{{  $user->name }}</li>
+                    <li class="list-group-item">{{  $user->surname }}</li>
+                    <li class="list-group-item">{{  $user->email }}</li>
+                    <li class="list-group-item">{{  $user->phone }}</li>
+                    <li class="list-group-item">{{  $user->cachet }} €</li>
+                </ul>
+            </div>
+        </div>
+
+        {{-- esperienza. --}}
+        <div class="col-12 mt-5 p-0">
+            <h3>Esperienza e descrizione personale</h3>
+
+            <div>
+                {{  $user->experience }}
+            </div>
+        </div>
+
+        {{-- bottone --}}
+        <div class="col-3 p-0">
+            <button type="button" class="mt-4 btn btn-outline-dark">
+                <a class="btn btn-light" href="{{route("admin.users.edit", $user->id)}}">Modifica il profilo</a>                       
+            </button>
+        </div>
+       
     </div>
 </div>
 
-<button type="button" class="mt-4">
-    <a class="btn btn-light" href="{{route("admin.users.edit", $user->id)}}">Modifica il profilo</a>                       
-</button>
+<div class="container-fluid mt-5">
+
+   
+</div>
+
 
 @endsection
