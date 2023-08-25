8938bf3d089c4400beafb45e6a98bfaace7b2827
MathiasChirivi <chirivimathias3@gmail.com>
2023-08-25 11:44:45 +0200

    Add index controller

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container-fluid mt-4">
     <div class="row justify-content-between">
-        <h2>Vista dettaglio User</h2>
+        <h2>Informazioni personali</h2>
         <h1>{{  $user->name }}</h1>
         <p>{{  $user->surname }}</p>
         <h2>{{  $user->email }}</h2>
