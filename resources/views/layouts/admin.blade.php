fe37c2617f757eb91a3a412cf4983b92ee255d68
davidetaribelli <davide.taribelli@gmail.com>
2023-08-28 22:52:04 +0200

    fix bug height container

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
 
 <body>
     <div id="app">
-        <div class="container-fluid bg_primary provaVH" id="provaVH" style="min-height: 100vh;">
+        <div class="container-fluid bg_primary min-vh-100">
             <div class="row">
      
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 sidebar rounded-5 m-3 bg_navbar">
