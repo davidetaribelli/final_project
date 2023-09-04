38f5be6f50e9a4886760469435500aa92134ad10
FedericoFinco <doppiaeffe94@gmail.com>
2023-09-04 18:01:58 +0200

    route and GenreController done

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\VoteController;
+use App\Http\Controllers\Api\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("/users", [UserController::class, "index"]);
+Route::get("/genres", [GenreController::class, "index"]);
 Route::get("/users/{id}", [UserController::class, "show"]);
 
 Route::post('/reviews', [ReviewController::class, 'post']);
