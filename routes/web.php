0d46a77ef307d13322504ad414fa8d4810895313
davidetaribelli <davide.taribelli@gmail.com>
2023-09-05 11:14:38 +0200

    fix bug message

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('/user/message', function () {
-    return view('admin.users.message');
-});
+
 
 
 
@@ -28,6 +26,10 @@ Route::middleware(['auth'])
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
+
+        Route::get('/user/message', function () {
+         return view('admin.users.message');
+        })->name('user.message');
         
 });
 
